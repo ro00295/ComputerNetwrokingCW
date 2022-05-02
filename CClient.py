91f5680942ce9dca0d59ca5bd13ee1c0dae3e5ba
ro00295 <104643295+ro00295@users.noreply.github.com>
2022-05-02 19:21:11 +0100

    Add files via upload

---
+++ CClient.py
@@ -0,0 +1,37 @@
+import socket
+import sys
+import time
+from socket import *
+
+pings = 1
+packet = ["candy", "chocolate", "gun", "hat", "toffee","coffee","tea","sweet","custard","bunties","choco"]
+#Send ping 10 times
+while pings < 11:
+
+    #Create a UDP socket
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    #Set a timeout value of 1 second
+    clientSocket.settimeout(1)
+
+    #Ping to server
+    message = (packet[pings]).encode("utf-8")
+
+    addr = ("127.0.0.1", 12001)
+
+    #Send ping
+    start = time.time()
+    clientSocket.sendto(message, addr)
+
+    #If data is received back from server, print
+    try:
+        data, server = clientSocket.recvfrom(1024)
+        end = time.time()
+        elapsed = end - start
+        print (data, " " , pings ," ", elapsed)
+
+    #If data is not received back from server, print it has timed out
+    except timeout:
+        print ('REQUEST TIMED OUT')
+
+    pings = pings + 1
